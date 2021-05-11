6b90aa7b34b4480741283d0b7132ace1fbcf338c
Mgot12602 <marcgotzens@hotmail.com>
2021-05-11 14:23:01 +0200

    added some built types like description and edited tags

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '80fdd014562e65d87579b5648a6917ac'),
+      secret: env("ADMIN_JWT_SECRET", "80fdd014562e65d87579b5648a6917ac"),
     },
   },
 });
